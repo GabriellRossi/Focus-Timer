035a1ff5225bf1a1f465f4143a389475c6b77d23
Gabriel Rossi <gabriel.rossi159@gmail.com>
2023-02-08 23:50:13 -0300

    criado função de countdown

--- script.js
+++ script.js
@@ -4,12 +4,31 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.sound-on')
 const buttonSoundOff = document.querySelector('.sound-off')
+let minutes
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+function countdown() {
+  setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent)
+
+    if (seconds <= 0) {
+      seconds = 60
+    }
+
+    secondsDisplay.textContent = seconds - 1
+
+    countdown()
+  }, 1000)
+}
 
 buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  countdown()
 })
 
 buttonPause.addEventListener('click', function () {
