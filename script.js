44bf8bd6f9070d5c24867d9ca8f1a1c7853c037c
Gabriel Rossi <gabriel.rossi159@gmail.com>
2023-02-10 00:06:12 -0300

    começando a contagem decrescente

--- script.js
+++ script.js
@@ -11,12 +11,26 @@ const secondsDisplay = document.querySelector('.seconds')
 function countdown() {
   setTimeout(function () {
     let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
 
-    if (seconds <= 0) {
-      seconds = 60
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, '0')
+
+    if (minutes <= 0) {
+      buttonPlay.classList.remove('hide')
+      buttonPause.classList.add('hide')
+      buttonSet.classList.remove('hide')
+      buttonStop.classList.add('hide')
+
+      return
     }
 
-    secondsDisplay.textContent = seconds - 1
+    if (seconds <= 0) {
+      seconds = 2
+
+      minutesDisplay.textContent = String(minutes - 1).padStart(2, '0')
+    }
+
+    secondsDisplay.textContent = String(seconds - 1).padStart(2, '0')
 
     countdown()
   }, 1000)
@@ -51,4 +65,9 @@ buttonSoundOff.addEventListener('click', function () {
 buttonSoundOn.addEventListener('click', function () {
   buttonSoundOn.classList.add('hide')
   buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function () {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = String(minutes).padStart(2, '0')
 })
