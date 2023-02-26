23793695da8c07ac76947ba2bae9d3a97773e297
Gabriel Rossi <gabriel.rossi159@gmail.com>
2023-02-26 19:24:28 -0300

    Refatorando controls e padronizando em factory

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-import resetControls from './controls.js'
-import { Timer } from './timer.js'
+import Controls from './controls.js'
+import Timer from './timer.js'
 
 const buttonPlay = document.querySelector('.play')
 const buttonPause = document.querySelector('.pause')
@@ -12,6 +12,13 @@ const secondsDisplay = document.querySelector('.seconds')
 let minutes = Number(minutesDisplay.textContent)
 let timerTimeOut
 
+const controls = Controls({
+  buttonPause,
+  buttonPlay,
+  buttonSet,
+  buttonStop
+})
+
 const timer = Timer({
   minutesDisplay,
   secondsDisplay,
@@ -20,22 +27,17 @@ const timer = Timer({
 })
 
 buttonPlay.addEventListener('click', function () {
-  buttonPlay.classList.add('hide')
-  buttonPause.classList.remove('hide')
-  buttonSet.classList.add('hide')
-  buttonStop.classList.remove('hide')
-
+  controls.play()
   timer.countdown()
 })
 
 buttonPause.addEventListener('click', function () {
-  buttonPause.classList.add('hide')
-  buttonPlay.classList.remove('hide')
+  controls.pause()
   clearTimeout(timerTimeOut)
 })
 
 buttonStop.addEventListener('click', function () {
-  resetControls()
+  controls.reset()
   timer.resetTimer()
 })
 
@@ -50,12 +52,12 @@ buttonSoundOn.addEventListener('click', function () {
 })
 
 buttonSet.addEventListener('click', function () {
-  let newMinutes = prompt('Quantos minutos?')
+  let newMinutes = controls.getMinutes()
   if (!newMinutes) {
-    timer.resetTimer()
+    timer.reset()
     return
   }
 
   minutes = newMinutes
-  updateTimerDisplay(minutes, 0)
+  timer.updateDisplay(minutes, 0)
 })
